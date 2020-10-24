7c42827f9f9c17e9848eb11ade9bdf6aed1f7317
Brad McAlister <brad.mcalister@sapphire-digital.com>
2020-10-24 17:19:13 -0500

    removing bad component

--- libs/shared/utils/src/lib/shared-utils.module.ts
+++ libs/shared/utils/src/lib/shared-utils.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormatterComponent } from './formatter/formatter.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FormatterComponent],
-  exports: [FormatterComponent],
+  declarations: [],
+  exports: [],
 })
 export class SharedUtilsModule {}
