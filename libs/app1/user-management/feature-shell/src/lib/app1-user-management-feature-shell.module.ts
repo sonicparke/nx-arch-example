5caf0642468eb6bfa5e738cc8d2436c3f1ea9547
Brad McAlister <brad.mcalister@sapphire-digital.com>
2020-10-24 16:37:13 -0500

    adding feature shell route component

--- libs/app1/user-management/feature-shell/src/lib/app1-user-management-feature-shell.module.ts
+++ libs/app1/user-management/feature-shell/src/lib/app1-user-management-feature-shell.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: '', pathMatch: 'full', component: UserManagementComponent },
     ]),
   ],
+  declarations: [UserManagementComponent],
+  exports: [UserManagementComponent],
 })
 export class App1UserManagementFeatureShellModule {}
