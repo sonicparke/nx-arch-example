f9482877d8e62b68d4db0a09f969eaf2cc7f7203
Brad McAlister <brad.mcalister@sapphire-digital.com>
2020-10-24 15:43:32 -0500

    initial commit

---
+++ apps/app1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-arch-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app1';
+}
